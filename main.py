202ecd421b25dd25394f5f0045c34f9660bbd6ff
Sergey Kirikeza <serj280294@yandex.ru>
2019-03-05 23:40:26 +0400

    First version of the application

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 import datetime
 
-from kivy.storage.dictstore import DictStore
+from kivy.storage.jsonstore import JsonStore
 
 Builder.load_string('''
 #:kivy 1.10.1
@@ -28,43 +28,74 @@ Builder.load_string('''
 		orientation: "vertical"
 
 		Label:
+			size_hint_y: .1
 			id: time
-			size_hint_y: None
-			size: self.texture_size
+			text_size: self.size
+			halign: 'center'
+    		valign: 'bottom'
 			font_size: 30
 			text: "00:00:00"
 
 		Label:
+			size_hint_y: .1
 			id: date
-			size_hint_y: None
-			size: self.texture_size
+			text_size: self.size
+			halign: 'center'
+    		valign: 'top'
 			font_size: 16
 			text: "00.00.0000"
 
-		Button:
-			text: "Test"
-			on_press: root.pressed()
+		BoxLayout:
+			orientation: 'vertical'
+				
+			Button:
+				text: "Awoke"
+				on_press: root.pressed(self.text)
+		
+			Button:
+				text: "Go to work"
+				on_press: root.pressed(self.text)
+	
+			Button:
+				text: "At work"
+				on_press: root.pressed(self.text)
+
+			Button:
+				text: "Go home"
+				on_press: root.pressed(self.text)
+
+			Button:
+				text: "In home"
+				on_press: root.pressed(self.text)
+
+			Button:
+				text: "Sleep"
+				on_press: root.pressed(self.text)
 
 ''')
 
 class MainScreen(Screen):
 
-	store = DictStore("app.txt")
-
+	store = JsonStore("data.json")
+	
 	def dateTimeUpdate(self, *args):
 		timeAndDate = datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y").split(" ")
 		self.ids.time.text, self.ids.date.text = timeAndDate
 
 
-	def pressed(self):
-		entrysIds= [int(entID) for entID in self.store.keys()]
-		print(entrysIds)
-		#newID =  entrysIds.max() if entrysIds else 0
+	def pressed(self, stateText):
+		entryNumbers = [number for number in self.store.keys()]
+		nextNumber = max(entryNumbers)+1 if entryNumbers else 0
 
+		state = stateText
+		date, time = datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y").split(" ")
 
-		#self.store.put(self.newID, time=datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y"))
+		self.store.put(nextNumber, date=date, time=time, state=state)
 		
-		#print(self.store.get('tito'))
+		#print(nextNumber, self.store.get(nextNumber))
+
+		#for data  in self.store:
+		#	print(data, self.store.get(data))
 
 class timeTrackingApp(App):
 	def build(self):
