fd7332afddf6081d8aecaa2504ffe1de8f1d0e37
Sergey Kirikeza <serj280294@yandex.ru>
2019-03-11 00:52:19 +0400

    Added label with last state information

--- main.py
+++ main.py
@@ -19,13 +19,62 @@ from kivy.clock import Clock
 import datetime
 
 from kivy.storage.jsonstore import JsonStore
+from kivy.factory import Factory
 
 Builder.load_string('''
 #:kivy 1.10.1
 
-<MainScreen>:
+<AppScreens@ScreenManager>:
+	MainScreen:
+	ViewScreen:
+
+<ViewScreen>:
+	name: "viewScr"
+
 	BoxLayout:
 		orientation: "vertical"
+		Button:
+			size_hint_y: None
+			height: dp(50)
+			text: "Back"
+			on_press: app.screen.current = "mainScr"
+
+		Label:
+			font_size: 30
+			size_hint_y: None
+			height: dp(50)
+			text: "All states"
+
+		#Label:
+		#	font_size: 40
+		#	text: "test"
+
+        RecycleView:
+            id: rv
+            key_viewclass: 'viewclass'
+
+            RecycleBoxLayout:
+                default_size: None, dp(20)
+                default_size_hint: 1, None
+                size_hint_y: None
+                height: self.minimum_height
+                orientation: 'vertical'
+
+<MainScreen>:
+	name: "mainScr"
+	BoxLayout:
+		orientation: 'vertical'
+
+		BoxLayout:
+			orientation: 'horizontal'
+			size_hint_y: None
+			height: dp(50)
+
+			Button:
+				text: "View all states"
+				on_press: app.screen.current = 'viewScr'
+			Button:
+				text: "Statistic"
 
 		Label:
 			size_hint_y: .1
@@ -45,6 +94,15 @@ Builder.load_string('''
 			font_size: 16
 			text: "00.00.0000"
 
+		Label:
+			id: lastStateLabel
+			size_hint_y: .05
+			text_size: self.size
+			halign: 'center'
+    		valign: 'center'
+			font_size: 16
+			text: root.getLastState()
+			
 		BoxLayout:
 			orientation: 'vertical'
 				
@@ -65,7 +123,7 @@ Builder.load_string('''
 				on_press: root.pressed(self.text)
 
 			Button:
-				text: "In home"
+				text: "At home"
 				on_press: root.pressed(self.text)
 
 			Button:
@@ -74,34 +132,59 @@ Builder.load_string('''
 
 ''')
 
+class ViewScreen(Screen):
+	
+	store = JsonStore("data.json")
+
+	def on_enter(self):
+		for entryNmb in self.store:
+			entry = self.store.get(entryNmb)
+			infoStr = entryNmb + ' ' + entry['date'] + ' ' + entry['time'] + ' ' + entry['state']
+			data = {"viewclass": "Label", "text": infoStr}
+			if data not in self.ids.rv.data:
+				self.ids.rv.data.append(data)
+
 class MainScreen(Screen):
 
 	store = JsonStore("data.json")
-	
+
 	def dateTimeUpdate(self, *args):
 		timeAndDate = datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y").split(" ")
 		self.ids.time.text, self.ids.date.text = timeAndDate
 
-
 	def pressed(self, stateText):
-		entryNumbers = [number for number in self.store.keys()]
-		nextNumber = max(entryNumbers)+1 if entryNumbers else 0
+		lastEntryNumber = self.getLastEntryNumber()
+		nextNumber = lastEntryNumber+1
 
 		state = stateText
 		date, time = datetime.datetime.today().strftime("%H:%M:%S %d.%m.%Y").split(" ")
 
-		self.store.put(nextNumber, date=date, time=time, state=state)
+		self.store.put(str(nextNumber), date=date, time=time, state=state)
 		
-		#print(nextNumber, self.store.get(nextNumber))
+		self.updateLastState(nextNumber)
 
-		#for data  in self.store:
-		#	print(data, self.store.get(data))
+	def getLastState(self):
+		lastEntryNumber = self.getLastEntryNumber()
+		if lastEntryNumber == 0:
+			return "Last state: not exist"
+		else:
+			return "Last state: " + self.store.get(str(lastEntryNumber))['state']
+
+	def updateLastState(self, entryNumber):
+		self.ids.lastStateLabel.text = "Last state: " + self.store.get(str(entryNumber))['state']
+
+	def getLastEntryNumber(self):
+		if (self.store):
+			return max([int(number) for number in self.store.keys()])
+		else:
+			return 0
 
 class timeTrackingApp(App):
+
 	def build(self):
-		startScreen = MainScreen()
-		Clock.schedule_interval(startScreen.dateTimeUpdate, 1)
-		return startScreen
+		self.screen = Factory.AppScreens()
+		#Clock.schedule_interval(startScreen.dateTimeUpdate, 1)
+		return self.screen
 
 if __name__ == "__main__":
     timeTrackingApp().run()
